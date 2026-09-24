55e61ca549ab2538f14c98642fd61472dd27484f
AlmostAPhysicist <almostaphysicist@gmail.com>
2026-09-24 01:07:45 -0400

    Updates

--- assets/themed-page-CbXQGeGj.js
+++ assets/themed-page-CbXQGeGj.js
@@ -13,7 +13,7 @@ import{n as e,t}from"./reactor-CUx62Pa_.js";function n(){let e=document.createEl
     <span class="star star--5"></span>
     <span class="star star--6"></span>
   </label>
-    `,e}function r(){let r=window.matchMedia(`(prefers-color-scheme: dark)`).matches,i=e(`light`),a=document.createElement(`div`);t(()=>{let e=i.value;a.className=`page ${e}`,c.checked=e===`dark`});let o=document.createElement(`div`);o.className=`toggle-wrapper-top-right`;let s=n(),c=s.querySelector(`input`);o.appendChild(s),c.addEventListener(`change`,()=>{i.value=c.checked?`dark`:`light`}),a.appendChild(o);let l=document.createElement(`div`);return l.className=`content`,l.innerHTML=`
+    `,e}function r(){let r=window.matchMedia(`(prefers-color-scheme: dark)`),i=e(r.matches?`dark`:`light`),a=document.createElement(`div`),o=document.createElement(`div`);o.className=`toggle-wrapper-top-right`;let s=n(),c=s.querySelector(`input`);t(()=>{let e=i.value;a.className=`page ${e}`,c.checked=e===`dark`}),o.appendChild(s),c.addEventListener(`change`,()=>{i.value=c.checked?`dark`:`light`}),a.appendChild(o);let l=document.createElement(`div`);return l.className=`content`,l.innerHTML=`
     <h1 style="margin-bottom: 20px;">Themed Page</h1>
     <p>Toggle the switch to change the theme.</p>
-  `,a.appendChild(l),window.matchMedia(`(prefers-color-scheme: dark)`).addEventListener(`change`,e=>{c.click()}),r&&c.click(),a}document.body.appendChild(r());
+  `,a.appendChild(l),r.addEventListener(`change`,e=>{i.value=e.matches?`dark`:`light`}),a}document.body.appendChild(r());
